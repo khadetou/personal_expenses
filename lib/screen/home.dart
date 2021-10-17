7bbca143ec94a04f334c4641773d606c8cc96c71
khadetou <khadetou96@gmail.com>
2021-10-17 18:02:30 +0000

    transaction & box styling

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "../models/transaction.dart";
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -8,16 +9,63 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "t1",
+      title: "New shoes",
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Weekly Groceries",
+      amount: 16.99,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Personal Expenses"),
+          title: const Text("Personal Expenses"),
         ),
         body: Column(
           children: [
-            Card(
-              child: Text('First card'),
+            const Card(
+              child: SizedBox(
+                child: Text('First card'),
+                width: 100,
+              ),
+              elevation: 10,
+            ),
+            Column(
+              children: transactions.map((transaction) {
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        transaction.amount.toString(),
+                      ),
+                    ),
+                    Column(children: <Widget>[
+                      Text(transaction.title),
+                      Text(transaction.date.toString()),
+                    ])
+                  ],
+                ));
+              }).toList(),
             )
           ],
         ));
